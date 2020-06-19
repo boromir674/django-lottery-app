2bca82de4b5eda29e4ffd7aa0e29f551f87e1d6e
Konstantinos <>
2020-06-19 22:22:49 +0200

    feat(dd_user): use DdUser as the AUTH_USER_MODEL and link Businesses to such entities

--- src/dd_lottery_project/dd_lottery_project/settings.py
+++ src/dd_lottery_project/dd_lottery_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'business_prizes',
     'lottery',
-    # 'lottery.lottery_admin'
+    'dd_app'
 ]
 
 MIDDLEWARE = [
@@ -150,6 +150,7 @@ REST_FRAMEWORK = {
 
 }
 
+AUTH_USER_MODEL = 'dd_app.DdUser'
 
 # APP SETTINGS
 
