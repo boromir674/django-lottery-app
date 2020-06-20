f31380c1232ea9487fff83bc701164347ea661e6
AimVoma <aim.voma@outlook.com>
2020-06-20 14:04:00 +0200

    enable CORS through corsheaders app

--- src/dd_lottery_project/dd_lottery_project/settings.py
+++ src/dd_lottery_project/dd_lottery_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'corsheaders', 
     'rest_framework.authtoken',
     'business_prizes',
     'lottery',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,6 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# CORS ORIGIN ALLOW
+CORS_ORIGIN_ALLOW_ALL = True   
 ROOT_URLCONF = 'dd_lottery_project.urls'
 
 TEMPLATES = [
